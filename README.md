c6ec9dc84b77d433bb49f6eb7668fc09dc9884c1
Pedro H Maschio <pedrohenriquermaschio@gmail.com>
2020-11-20 10:30:11 -0300

    v0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# JogoFinal
+Jogo final da disciplina Fundamentos de Programação CC IMED
---
+++ main.py
@@ -0,0 +1,18 @@
+#Jogo desvie das notas ruins
+import pygame
+import random
+import time
+
+pygame.init()
+relogio = pygame.time.Clock()
+pygame.pygame.set_caption("Tire apenas A ou B")
+
+icon = pygame.image.load("assets/icon.png")
+pygame.display.set_icon(icon)
+
+largura = 1080
+altura = 720
+
+display = pygame.pygame.set_mode((largura,altura))
+background = pygame.image.load("assets/escolinha.png")
+aluno = pygame.image.load("assets/aluno.png")
